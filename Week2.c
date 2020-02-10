124b2b3c4a2c70f8e79a86dd25f109190b59764d
pachara <pachara.sr@ku.th>
2020-02-11 00:29:24 +0900

    Types, Operators, and Expressions

---
+++ Week2.c
@@ -0,0 +1,33 @@
+//char array[n] เก็บค่าได้   n-1 charecters ถ้าเก็บเกินจะไปเอาค่าอื่นๆมาแสดงแทน
+    
+    
+    // gets(name); // รับ stringทั้งแถวแม้จะมีspaceและเอาstringเก็บไว้ใน char array
+    // fgets(name, n, stdin); // อ่านค่าได้จำนวน n-1charectersถ้าเกินจะไม่เก็บที่เกิน ถ้ามีช่องว่างเหลือจะกลายเป็น\nทำให้ขึ้นบรรทัดใหม่อัตโนมัติ
+    // getchar(chr); // รับแค่charecter เดียว
+    
+    
+    // atoi = Array to Integer 
+    // atol = Array to Long 
+    // atof = Array to Float
+    // ทั้ง 3 ตัวต้องมีตัวแปรมารับค่า
+
+
+    //puts("string");  แสดงค่าเสร็จขึ้นบรรทัดใหม่
+    //putchar('charecter'); แสดงแค่ตัวอักษรเดียว
+    //printf("%d,f,s,c",variable); แสดงตามค่าที่เก็บไว้ไม่ขึ้นบรรทัดใหม่ให้ต้องใช้ \n
+
+    
+    //   ถ้ากำหนดค่าตัวแปรเป็น 0 จะให้ค่าเป็น false ทั้งใน int float char
+    
+    // const ใช้ฟิกค่าตัวแปรไม่ให้เปลี่ยน
+    
+    // int,float = 4 bytes
+    // char      = 1 byte
+    // double    = 8 bytes
+    
+    // sizeof ตรวจสอบขนาดตัวแปร return เป็นหน่วย byte
+    
+    // %o ฐาน  8, %x ฐาน 16 ,%c charecter , %s string
+    
+    //BITWISE OPERATOR
+    // & = and  , | = or , ^ = XOR , << = lshift , >> = rshift , ~ one's compliment 
