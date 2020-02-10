056d007fee6250579e2637490e7412bfd91f6849
pachara <pachara.sr@ku.th>
2020-02-11 00:58:25 +0900

    Control Flow

---
+++ week3.c
@@ -0,0 +1,37 @@
+
+    
+    /* if(___) statement
+       else (____)  statement  โดยelseจะคู่กับifที่ใกล้สุดแล้วห้ามมีอย่างอื่นมาขั้นระหว่างกัน 
+       else if(____) จะทำเมื่อ if เป็นเท็จและเงื่อนไขต้องตรงกันด้วยถึงจะทำงาน*/
+    
+    // while (______){} จะทำจนกว่าเงื่อนไขจะเท็จ
+    /* count = 5;    
+    count++ * count = 5*6   เพราะตัวcount++จะ +ค่าcountไป1 แต่จะ  return ค่าก่อนถูกบวกกลับ
+    ++count * count = 6*6   เพราะตัว++count จะ +ค่าcountwx 1และ returnค่าที่ถูกบวกไปแล้วด้วย */
+    
+    /* do {
+     
+     statement;
+        
+    }
+    while(____);
+
+    //SWITCH CASE
+    /*switch (expression) {
+case const-expr:
+statements
+case const-expr:
+statements
+default:
+statements
+}
+สำหรับ default ไม่จำเป็นต้องมีจะทำงานเมื่อไม่เข้า case ไหนเลย และถ้าไม่ใส่ break ในแต่ละ case เมื่อเข้าเงื่อนไข case ไหนจะไล่ทำงานตั้งแต่ case นั้นลงมาจนหมด */
+    
+    
+    
+    // for (1; 2; 3){ // ถ้าเปลี่ยน j < i เป็น 1 จะกลายเป็น Infinite Loop // 1 คือ True 0 = fault
+    // จะทำ1ก่อนครั้งแรกและคร้้งเดียวแล้ว 2 แล้ว ไปทำในลูปเสร็จรอบหนึ่งทำ3แล้วมาเช็คเงื่อนไขตรง 2 แล้วเข้าลูปอีกรอบแล้ว ทำ 3 ->2->statement
+    
+    
+    
+   
